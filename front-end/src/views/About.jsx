50862bca7b4801327a2ab377e6aad39ed40cb6b0
Suleman <suleman.khn10@gmail.com>
2021-06-30 14:22:52 +0200

    modif about contact addmovies
    done

--- front-end/src/views/About.jsx
+++ front-end/src/views/About.jsx
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-            <div className="container">
-                <h1 className="text-center">About</h1>
+            <div>
+
+                <div className="container">
+                    <h1 className="text-center pt-5">Final project with Konexio</h1>
+                    <h3 className=" text-info text-center pt-5">Attendees</h3>
+                    <h3 className="text-center pt-1">Suleman, Lucas, Mehdi, Faruk</h3>
+                </div>
+
+                <div className="container">
+                    <h3 className="text-info text-center pt-5">Languages and frameworks</h3>
+                    <h3 className="text-center pt-1">HTML/CSS, Javascript: Node and React</h3>
+                </div>
             </div>
         )
     }
--- front-end/src/views/AddMovies.jsx
+++ front-end/src/views/AddMovies.jsx
@@ -3,10 +3,19 @@ import React, { Component } from 'react'
 export default class AddMovies extends Component {
     render() {
         return (
-            <div className="container">
-            <h1 className="text-center">Add movies</h1>
-        </div>
-        
+
+            <div>
+
+                <div className="container">
+                    <h1 className="text-center pt-5">Add latest movies here !</h1>
+                    <div class="input-group pt-5">
+                        <input type="text" class="form-control" placeholder="Add movie name" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        </input>
+                        <button class="btn btn-outline-primary" type="button" id="button-addon2">Add</button>
+                    </div>
+                </div>
+            </div>
+
         )
     }
 }
