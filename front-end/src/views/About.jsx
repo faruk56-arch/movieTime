b134e0e936d0d31cb4abf6e417d8ba5331adb23e
Suleman <suleman.khn10@gmail.com>
2021-06-30 10:48:20 +0200

    full stack movieTime

---
+++ front-end/src/views/About.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="text-center">About</h1>
+            </div>
+        )
+    }
+}
---
+++ front-end/src/views/Homepage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="text-center">Homepage</h1>
+            </div>
+
+        )
+    }
+}
