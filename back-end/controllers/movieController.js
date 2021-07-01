f975d19c427e3a97afcdec4df5372761f195f2f9
l <albert.lucass@hotmail.com>
2021-07-01 10:37:52 +0200

    backend finis

--- back-end/controllers/movieController.js
+++ back-end/controllers/movieController.js
@@ -1,9 +1,14 @@
 const movieModel = require("../models/movie")
+const authorModel = require("../models/author")
+const actorModel = require("../models/actor")
+const adressModel = require("../models/adress")
+const genreModel = require("../models/genre")
+const originalLanguageModel = require("../models/originalLanguage")
 const path = require("path")
 
 const getMovies = async (req, res) => {
     try {
-        const movies = await movieModel.find()
+        const movies = await movieModel.find().populate(['actor', 'adress', 'author', 'genre', 'originalLanguage'])
 
         res.json(movies)
     } catch (error) {
@@ -16,25 +21,43 @@ const getMovie = async (req, res) => {
     try {
         const titlemovie = req.params.title
         const movie = await movieModel.findOne(titlemovie)
+            .populate("actor", { name: 1, _id: 0 })
+            .populate("adress", { typeName: 1, adress: 1, _id: 0 })
+            .populate("author", { name: 1, _id: 0 })
+            .populate("genre", { name: 1, _id: 0 })
+            .populate("originalLanguage", { name: 1, _id: 0 }).select({
+                _id: 0,
+                title: 1,
+                description: 1,
+                image: 1,
+                releaseDate: 1,
+                note: 1,
+                voteCount: 1,
+                adult: 1
+            })
+
+
+        // (['actor', 'adress', 'author', 'genre', 'originalLanguage'])
 
         res.json(movie)
     } catch (error) {
-
+        console.log(error);
         res.status(500).json({ message: "There was a problem", error })
     }
 }
 
 const addMovie = async (req, res) => {
-    try {
-        if (!errors.isEmpty()) {
-            res.status(400).json({ errors: errors.array() })
-        } else {
-            const newMovie = await movieModel.create(req.body)
 
-            res.json({ message: "The movie was added!", newMovie })
-        }
+    try {
+
+        const newMovie = await movieModel.create(req.body)
+        console.log(newMovie);
+        res.json({ message: "The movie was added!", newMovie })
+
     } catch (error) {
+        console.log("error found", error);
         res.status(500).json({ message: "There was a problem", error })
+
     }
 }
 
