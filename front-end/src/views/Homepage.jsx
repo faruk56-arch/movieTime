5c6b7f30cfffdd27765bcee2de24e486e3fb3bdb
l <albert.lucass@hotmail.com>
2021-07-08 12:12:55 +0200

    Fix problems made by Faruk

--- front-end/src/views/Homepage.jsx
+++ front-end/src/views/Homepage.jsx
@@ -36,16 +36,10 @@ export default class Homepage extends Component {
     }
 
     render() {
-        console.log(this.state.movies);
         if (this.state.movies.length > 0) {
             return (
                 <div>
                     <Search getName={this.getName}/>
-                    <div class="input-group mb-3 pt-5">
-                        <input type="text" class="form-control" placeholder="Type a movie's name" aria-label="Recipient's username" aria-describedby="button-addon2">
-                        </input>
-                        <button class="btn btn-outline-primary" type="button" id="button-addon2">Search</button>
-                    </div>
                     <div className = "container-card"> 
                         {this.state.movies.map(movie => {
                             return <Card
