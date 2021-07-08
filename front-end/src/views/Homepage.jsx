cb74b542d96dbfcd303c9133106bbf4dc66c3f88
l <albert.lucass@hotmail.com>
2021-07-08 14:01:31 +0200

    More Fix

--- front-end/src/views/Homepage.jsx
+++ front-end/src/views/Homepage.jsx
@@ -2,39 +2,29 @@ import React, { Component } from 'react'
 import Search from '../views/Search'
 import Card from "../components/Card"
 import "./homepage.css"
-
 export default class Homepage extends Component {
-
     constructor() {
         super()
-
         this.state = {
             movies: []
         }
-
     }
-
     componentDidMount() {
         this.getMovies()
     }
-
     getMovies() {
         // console.log("getCountry", country);
-
         const url = "http://localhost:8000/movies/10"
-
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 // console.log("data in getCountry ", data);
-
                 this.setState({
                     movies: data
                 })
             })
     }
-
     render() {
         if (this.state.movies.length > 0) {
             return (
--- front-end/src/views/Search.jsx
+++ front-end/src/views/Search.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 export default class Search extends Component {
     constructor() {
         super()
@@ -21,62 +20,44 @@ export default class Search extends Component {
                     this.setState({
                         filmTrouver: result
                     })
-
                 } else {
                     this.setState({
                         filmTrouver: {}
                     })
                 }
-
             })
     }
-
-
     render() {
         console.log("MovieName:", this.state.name)
         console.log("filmTrouver", this.state.filmTrouver)
-
+        console.log(this.state.filmTrouver)
         return (
-
             <div class="input-group mb-3 pt-5">
                 <input type="text" class="form-control" placeholder="Type a movie's name" aria-label="Recipient's username" aria-describedby="button-addon2" onChange={this.changeName}>
                 </input>
                 <button class="btn btn-outline-primary" type="button" id="button-addon2" onClick={this.getName}>Search</button>
                 <div className="container">
-
-
                     {
-
                         Object.keys(this.state.filmTrouver).length > 0 ? (
-
                             <div>
-                                <p>Title = {filmTrouver.title}</p>
-                                <p>Description = {filmTrouver.description}</p>
-                                <p>Adult Movie = {filmTrouver.adult ? "✓" : "X"}</p>
-                                <img src={filmTrouver.image} />
-                                <p>Note = {filmTrouver.note}</p>
-                                <p>Number of votes so far = {filmTrouver.voteCount}</p>
-                                <p>Genre(s) = {filmTrouver.genre.map(genre => genre.name).join(", ")}</p>
-                                <p>Author(s) = {filmTrouver.author.map(author => author.name).join(", ")}</p>
-                                <p>Actor(s) = {filmTrouver.actor.map(actor => actor.name).join(", ")}</p>
-                                <p>Support = {filmTrouver.adress.map(adress => adress.type).join(", ")}</p>
-                                <p>Name = {filmTrouver.adress.map(adress => adress.typeName).join(", ")}</p>
-                                <p>Where to find = {filmTrouver.adress.map(adress => adress.adress).join(", ")}</p>
-                                <p>Original Language = {filmTrouver.originalLanguage.map(originalLanguage => originalLanguage.name)}</p>
+                                <p>Title = {this.state.filmTrouver.title}</p>
+                                <p>Description = {this.state.filmTrouver.description}</p>
+                                <p>Adult Movie = {this.state.filmTrouver.adult ? "✓" : "X"}</p>
+                                <img src={this.state.filmTrouver.image} />
+                                <p>Note = {this.state.filmTrouver.note}</p>
+                                <p>Number of votes so far = {this.state.filmTrouver.voteCount}</p>
+                                <p>Genre(s) = {this.state.filmTrouver.genre.map(genre => genre.name).join(", ")}</p>
+                                <p>Author(s) = {this.state.filmTrouver.author.map(author => author.name).join(", ")}</p>
+                                <p>Actor(s) = {this.state.filmTrouver.actor.map(actor => actor.name).join(", ")}</p>
+                                <p>Support = {this.state.filmTrouver.adress.map(adress => adress.type).join(", ")}</p>
+                                <p>Name = {this.state.filmTrouver.adress.map(adress => adress.typeName).join(", ")}</p>
+                                <p>Where to find = {this.state.filmTrouver.adress.map(adress => adress.adress).join(", ")}</p>
+                                <p>Original Language = {this.state.filmTrouver.originalLanguage.map(originalLanguage => originalLanguage.name)}</p>
                             </div>
                         ) : "Entrer un film a rechercher"
                     }
-
-
-
-
-
-
                 </div>
             </div>
         )
-
     }
-
 }
-
