2ec4d401c6107e45569e229ceff81ee37ba08148
l <albert.lucass@hotmail.com>
2021-07-12 15:22:46 +0200

    Search Done

--- front-end/src/components/Card2.jsx
+++ front-end/src/components/Card2.jsx
@@ -7,18 +7,19 @@ class Card2 extends React.Component {
             <div className="Card2">
 
                 <img src={this.props.image} />
-                <p> title:{this.props.title}</p>
-                <p> description:{this.props.description}</p>
-                <p> adult:{this.props.adult ? 'yes' : 'no'}</p>
-                <p>Note:{this.props.note}</p>
-                <p> voteCount:{this.props.voteCount}</p>
-                <p> genre:{this.props.genre}</p>
-                <p>author:{this.props.author}</p>
-                <p>actor:{this.props.actor}</p>
-                <p>releaseDate:{this.props.releaseDate}</p>
-                <p>adress:{this.props.adress}</p>
-                <p> originalLanguage:{this.props.originalLanguage}</p>
-
+                <p> Title :  {this.props.title}</p>
+                <p> Description :  {this.props.description}</p>
+                <p> Adult :  {this.props.adult ? 'yes' : 'no'}</p>
+                <p>Note :  {this.props.note}</p>
+                <p>Number of votes so far :  {this.props.voteCount}</p>
+                <p>Genres :  {this.props.genre}</p>
+                <p>Authors :  {this.props.author}</p>
+                <p>Actors :  {this.props.actor}</p>
+                <p>Release Date :  {this.props.releaseDate}</p>
+                <p>Support :  {this.props.support}</p>
+                <p>Name :  {this.props.name}</p>
+                <p>Where to find : {this.props.whereToFind}</p>
+                <p>Original Language :  {this.props.originalLanguage}</p>
 
             </div>
         )
